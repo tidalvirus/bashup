833cd2e2a5db025d7e73308bcd368b38dbc248bc
Siraj 'Sid' Rakhada <sid-git@mindless.co.uk>
2015-06-02 18:38:10 +0800

    Added sounds, updated docs, added pause feature

--- README.md
+++ README.md
@@ -6,13 +6,16 @@ This is so it can't be exited by accident.
 
 ## What does it do?
 
-* Bounces a square on the screen off the edges. Colour can be changed, as can size and speed.
+* Bounces a square on the screen off the edges. Colour can be changed, as can size and speed. Beeps (randomly) when the box hits the edge.
+* Plays music (midi files from a pygame tetromino example)
 
 ## Commands
 
 * F - toggle FPS (frames per second) display
 * left/right arrow - change size of the square
 * up/down arrow - change speed of the square
+* M - toggle playing music - random tracks
+* P - 'Pause' the game
 * space bar - the square changes colour
 
 ## Requirements
@@ -28,13 +31,20 @@ That should be it.
 
 Hopefully, when I get time:
 
-* Fade between colours
-* Leave a limited trail - configurable to be longer or shorter
+* Fade between colours.
+* Leave a limited trail - configurable to be longer or shorter.
 * Display more details - speed, FPS, directions, etc.
-* Make a sound when it hits the edge
-* Display pictures instead of a square - change the picture on each bounce, or press of the space bar instead
-of colours
-* Add more bouncing items on screen
+* Make a sound when it hits the edge - done 20150602.
+* Display pictures instead of a square - change the picture on each bounce,
+or press of the space bar instead of colours.
+* Add more bouncing items on screen.
+
+## Issues
+
+* Beep is delayed from when the box hits the edge!
+I've got no idea why, but on a Pi, the sound comes up later than when testing
+the same thing on an old MacBook Air - which sometimes can't do the same speed
+of graphics that the Pi can!
 
 ## The code is awful!
 
--- bashup.py
+++ bashup.py
@@ -6,24 +6,44 @@
 # left/right to change size
 # space to change colour
 # f to display FPS
-# IMPORTANT! ESC and p together to quit
+# m to start/stop music - randomly
+# p to pause
+# IMPORTANT! ESC and l together to quit
 
 import sys, pygame
 import random
 from pygame.locals import *
 
-black = 0, 0, 0
-background = black
+#               R    G    B
+WHITE       = (255, 255, 255)
+GRAY        = (185, 185, 185)
+BLACK       = (  0,   0,   0)
+RED         = (155,   0,   0)
+LIGHTRED    = (175,  20,  20)
+GREEN       = (  0, 155,   0)
+LIGHTGREEN  = ( 20, 175,  20)
+BLUE        = (  0,   0, 155)
+LIGHTBLUE   = ( 20,  20, 175)
+YELLOW      = (155, 155,   0)
+LIGHTYELLOW = (175, 175,  20)
+
+BGCOLOR = BLACK
+TEXTCOLOR = WHITE
+TEXTSHADOWCOLOR = GRAY
+
+TUNES = ['tetrisb.mid', 'tetrisc.mid']
+BEEPLIST = ['beep1.ogg', 'beep2.ogg', 'beep3.ogg', 'beep4.ogg']
 
 global direction, dirty_rects
 
 direction=0 # store the direction of the rectangle travel, 0 = right, 1 = left
 
 def main():
-	global screen, WIDTH, HEIGHT, bgsurf
-	global dirty_rects, oldText, dx, dy, speed, oldsize
+	global screen, WIDTH, HEIGHT, bgsurf, BIGFONT, BASICFONT, BEEPS
+	global dirty_rects, oldText, dx, dy, speed, oldsize, clock
 	oldText = None
 	dirty_rects = []
+	BEEPS = []
 	displayFPS = 1
 	fpscleared = 0
 	playtime = 0
@@ -42,21 +62,33 @@ def main():
 	#pygame.display.set_caption('Hello World!')
 	WIDTH=screen.get_width()
 	HEIGHT=screen.get_height()
-	bgsurf = pygame.Surface(screen.get_size())
-	bgsurf = bgsurf.convert()
-	bgsurf.fill(background)
+	BASICFONT = pygame.font.Font(None, 18)
+	BIGFONT = pygame.font.Font(None, 100)
+	#bgsurf = pygame.Surface(screen.get_size())
+	#bgsurf = bgsurf.convert()
+	#bgsurf.fill(BGCOLOR)
+	screen.fill(BGCOLOR)
 	pygame.key.set_repeat(500, 30)
+	# load the sound files
+	for i in range(len(BEEPLIST)):
+		BEEPS.append(pygame.mixer.Sound(BEEPLIST[i]))
 
 	curX = curY = 0
 
-	screen.fill(black)
+
+	playing = None
+	screen.fill(BGCOLOR)
 	pygame.display.flip()
+
+	for i in range(len(BEEPS)):
+		BEEPS[i].play()
+
 	while True:
 		milliseconds = clock.tick(FPS)
 		seconds = milliseconds / 1000.0
 		playtime += seconds
 		keys = pygame.key.get_pressed()
-		if keys[K_ESCAPE] and keys[K_p]:
+		if keys[K_ESCAPE] and keys[K_l]:
 			terminate()
 		for event in pygame.event.get():
 			if event.type == QUIT:
@@ -93,10 +125,22 @@ def main():
 					# toggle fps display
 					displayFPS = not displayFPS
 					if fpscleared == 0:
-						pygame.draw.rect(screen, background, pygame.Rect(oldText))
+						pygame.draw.rect(screen, BGCOLOR, pygame.Rect(oldText))
 						fpscleared = 1
 					if fpscleared == 1 and displayFPS == 1:
 						fpscleared = 0
+				if event.key == pygame.K_m:
+					if playing == None:
+						pygame.mixer.music.load(TUNES[random.randint(0,len(TUNES)-1)])
+						pygame.mixer.music.play(-1, 0.0)
+						playing = True
+					else:
+						pygame.mixer.music.stop()
+						playing = None
+				if event.key == pygame.K_p:
+					# pause
+					showTextScreen("PAUSED")
+
 
 		if displayFPS and seconds % 1:
 			# run FPS display code
@@ -117,11 +161,11 @@ def terminate():
 def displayFPStext(fps):
 	global oldText
 	fpsfont = pygame.font.Font(None, 36)
-	text = fpsfont.render(fps, 1, (0,0,255), background)
+	text = fpsfont.render(fps, 1, (0,0,255), BGCOLOR)
 	if oldText == None:
 		oldText = text.get_rect(centerx=WIDTH/2)
 	textrect = pygame.Rect(oldText)
-	pygame.draw.rect(screen, background, textrect)
+	pygame.draw.rect(screen, BGCOLOR, textrect)
 	dirty_rects.append(textrect)
 
 	textrect = text.get_rect(centerx=WIDTH/2)
@@ -132,7 +176,8 @@ def displayFPStext(fps):
 	return
 
 def moveRectangle(x, y, size, colour, seconds, speed):
-	global dx, dy, oldsize
+	global dx, dy, oldsize, BEEPS
+	sound = None
 	distance = HEIGHT
 	if dx == None:
 		dx = dy = speed # set initial speed - pixels per second
@@ -140,10 +185,12 @@ def moveRectangle(x, y, size, colour, seconds, speed):
 		# then we've had a size change
 		rect = pygame.Rect(x, y, oldsize, oldsize)
 		oldsize=size
+		sound = BEEPS[0]
+		sound.play()
 	else:
 		rect = pygame.Rect(x, y, size, size)
 
-	pygame.draw.rect(screen, background, rect)
+	pygame.draw.rect(screen, BGCOLOR, rect)
 	dirty_rects.append(rect)
 
 
@@ -154,18 +201,25 @@ def moveRectangle(x, y, size, colour, seconds, speed):
 		x = 0
 		dx *= -1
 		dx += random.randint(-15,15)
+		sound = BEEPS[random.randint(0,len(BEEPS)-1)]
 	elif x + size >= WIDTH:
 		x = WIDTH - size
 		dx *= -1
 		dx += random.randint(-15,15)
+		sound = BEEPS[random.randint(0,len(BEEPS)-1)]
 	if y < 0:
 		y = 0
 		dy *= -1
 		dy += random.randint(-15,15)
+		sound = BEEPS[random.randint(0,len(BEEPS)-1)]
 	elif y + size >= HEIGHT:
 		y = HEIGHT - size
 		dy *= -1
 		dy += random.randint(-15,15)
+		sound = BEEPS[random.randint(0,len(BEEPS)-1)]
+
+	if sound != None:
+		sound.play()
 
 	rect = pygame.Rect(x, y, size, size)
 	pygame.draw.rect(screen, colour, rect)
@@ -174,6 +228,47 @@ def moveRectangle(x, y, size, colour, seconds, speed):
 
 	return (x, y)
 
+# taken from tetromino demo
+def showTextScreen(text):
+	# This function displays large text in the
+	# center of the screen until a key is pressed.
+	# Draw the text drop shadow
+	titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTSHADOWCOLOR)
+	titleRect.center = (int(WIDTH / 2), int(HEIGHT / 2))
+	screen.blit(titleSurf, titleRect)
+
+	# Draw the text
+	titleSurf, titleRect = makeTextObjs(text, BIGFONT, TEXTCOLOR)
+	titleRect.center = (int(WIDTH / 2) - 3, int(HEIGHT / 2) - 3)
+	screen.blit(titleSurf, titleRect)
+
+	# Draw the additional "Press a key to play." text.
+	pressKeySurf, pressKeyRect = makeTextObjs('Press a key to play.', BASICFONT, TEXTCOLOR)
+	pressKeyRect.center = (int(WIDTH / 2), int(HEIGHT / 2) + 100)
+	screen.blit(pressKeySurf, pressKeyRect)
+
+	while checkForKeyPress() == None:
+		pygame.display.update()
+		clock.tick()
+
+	screen.fill(BGCOLOR)
+	pygame.display.update()
+
+def makeTextObjs(text, font, color):
+	surf = font.render(text, True, color)
+	return surf, surf.get_rect()
+
+def checkForKeyPress():
+    for event in pygame.event.get():
+        if event.type == QUIT:      #event is quit
+            terminate()
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:   #event is escape key
+                terminate()
+            else:
+                return event.key   #key found return with it
+    # no quit or key events in queue so return None
+    return None
 
 if __name__ == '__main__':
 	main()
